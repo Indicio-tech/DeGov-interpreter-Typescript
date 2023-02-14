3191516a03dbbffb8472160cc845bc5a9bbf8812
KolbyRKunz <kolbykunz@yahoo.com>
2023-02-14 11:31:43 -0700

    small fixes and applying formatting
    Signed-off-by: KolbyRKunz <kolbykunz@yahoo.com>

--- src/types/Governance.ts
+++ src/types/Governance.ts
@@ -1,49 +1,41 @@
 export interface GovernanceFile {
-    '@context': string[]
-    author: string
-    name: string
-    version: string
-    format: string
-    id: UUID
-    uri: string
-    description: string
-    last_updated: number | Date
-    ttl: number
-    docs_uri: string
-    schemas: Schema[]
-    participants: Participant
-    [property: string | number | symbol]: any
+  "@context": string[];
+  author: string;
+  name: string;
+  version: string;
+  format: string;
+  id: UUID;
+  uri: string;
+  description: string;
+  last_updated: number | Date;
+  ttl: number;
+  docs_uri: string;
+  schemas: Schema[];
+  participants: Participant;
+  [property: string | number | symbol]: any;
 }
 
 export interface Schema {
-    id: SchemaId
-    name: string
+  id: SchemaId;
+  name: string;
 }
 
-export interface Participant  {
-    id: UUID
-    author: string
-    created: Date | number
-    version: string | number
-    topic: string
-    entries: {[schemaUri: JsonURI] : Entry}
+export interface Participant {
+  id: UUID;
+  author: string;
+  created: Date | number;
+  version: string | number;
+  topic: string;
+  entries: { [schemaUri: JsonURI]: Entry };
 }
 
 export interface Entry {
-    [did: string] : any 
-    // {
-    //     roles : string[]
-    // } | {
-    //     name: string 
-    //     website: string
-    //     email: string
-    //     phone: string
-    // }
+  [did: string]: {};
 }
 
 // Testing out pattern string types
-type SchemaId = `${string}:${number}:${string}:${number}`
+type SchemaId = `${string}:${number}:${string}:${number}`;
 
-type UUID = `${string}-${string}-${string}-${string}-${string}`
+type UUID = `${string}-${string}-${string}-${string}-${string}`;
 
-export type JsonURI = `${'http://' | 'https://'}${string}${'.json'}`
+export type JsonURI = `${"http://" | "https://"}${string}${".json"}`;
